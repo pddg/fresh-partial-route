b10354184846f79badf01c116709820a3927c824
Shoma Kokuryo <s.kokuryo@gmail.com>
2025-06-21 15:10:19 +0900

    fix

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,9 +4,9 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $bar from "./routes/bar.tsx";
+import * as $foo from "./routes/foo.tsx";
 import * as $index from "./routes/index.tsx";
-import * as $partials_bar from "./routes/partials/bar.tsx";
-import * as $partials_foo from "./routes/partials/foo.tsx";
 
 import type { Manifest } from "$fresh/server.ts";
 
@@ -14,9 +14,9 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/bar.tsx": $bar,
+    "./routes/foo.tsx": $foo,
     "./routes/index.tsx": $index,
-    "./routes/partials/bar.tsx": $partials_bar,
-    "./routes/partials/foo.tsx": $partials_foo,
   },
   islands: {},
   baseUrl: import.meta.url,
